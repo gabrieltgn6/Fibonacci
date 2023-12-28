f00cef3fafd9f4cdeb7a86353d1c08a46345a75c
Batonazu <gabrieltgn6@gmail.com>
2023-12-28 12:18:33 +0100

    no funciona la terminal para hacer push, pruebo por aqui

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,31 @@
+
+
 # Programa para calcular fibonacci a partir de un numero dado
 
 datos = int(input("Que numero quieres calcular?"))
 
+a = 0
+b = 1
+
 for i in range (datos):
-    a = 1
-    b = a
-    resultado = b + a
-    a = resultado
 
-else:
-    print = resultado
+    a = b
+    b = a + b    
 
+
+print = b
+
+"""
+
+datos = int(input("¿Qué número quieres calcular en la serie de Fibonacci? "))
+
+# Inicializamos las variables de la serie de Fibonacci
+a, b = 0, 1
+
+# Mostramos los primeros "datos" números de la serie de Fibonacci
+for i in range(datos):
+    print(a, end=" ")
+
+    # Calculamos el siguiente número en la serie
+    a, b = b, a + b
+"""
