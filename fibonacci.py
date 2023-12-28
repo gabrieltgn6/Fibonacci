e7ae61ce60b3cd60e9f30907fddd9f699d3fcdf8
Batonazu <gabrieltgn6@gmail.com>
2023-12-28 12:06:56 +0100

    Primera prueba. No devuelve el valor

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# Programa para calcular fibonacci a partir de un numero dado
+
+datos = int(input("Que numero quieres calcular?"))
+
+for i in range (datos):
+    a = 1
+    b = a
+    resultado = b + a
+    a = resultado
+
+else:
+    print = resultado
+
